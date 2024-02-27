627ed1185084938a634fe4c8a44e1aa96f8bef15
RHW18 <943359178@qq.com>
2024-02-27 22:42:25 +0800

    vue3项目环境搭建

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
-<template>
-  <div class="common-layout">
-    <meta name="referrer" content="no-referrer" />
-    <el-container>
-      <el-header height="75">
-        <heaterTitle/>
-      </el-header>
-      <el-main>
-        <indexVideoGirdPage/>
-      </el-main>
-    </el-container>
-  </div>
-</template>
-
-<script>
-import heaterTitle from './components/title.vue'
-import indexVideoGirdPage from './components/page/indexVideoGirdPage.vue'
-
-export default {
-  name: 'App',
-  components: {
-    heaterTitle, indexVideoGirdPage
-  },
-}
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
 
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
